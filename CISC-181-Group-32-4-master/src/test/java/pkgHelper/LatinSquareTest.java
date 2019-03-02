a82dad6efe4392512c8c713667f68b4d6509ee16
ryanw <ryanw@128.4.97.180>
2019-03-02 14:24:54 -0500

    Added test cases for hasAllValues.

--- CISC-181-Group-32-4-master/src/test/java/pkgHelper/LatinSquareTest.java
+++ CISC-181-Group-32-4-master/src/test/java/pkgHelper/LatinSquareTest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 
 import org.junit.Test;
+/*Tests completed:
+ * doesElementExistTests 1,2
+ */
 
 public class LatinSquareTest {
 
@@ -46,6 +49,29 @@ public class LatinSquareTest {
 		assertEquals(bExpected, bActual);
 	}
 	
+	@Test //Added by Ryan
+	public void hasAllValues_Test1() {
+		int [] tarr1 = {1,2,3,4,5};
+		int [] tarr2 = {1,2,3,4,5};
+		boolean expectedBool = true;
+		boolean actualBool = LatinSquare.hasAllValues(tarr1, tarr2);
+		
+		assertEquals(expectedBool,actualBool);
+		
+	}
+	@Test //Added by Ryan
+	public void hasAllValues_Test2() {
+		int [] tarr1 = {1,2,3,4};
+		int [] tarr2 = {1,2,3,6,5};
+		boolean expectedBool = false;
+		boolean actualBool = LatinSquare.hasAllValues(tarr1, tarr2);
+		
+		assertEquals(expectedBool,actualBool);
+		
+	}
+	
 	
 	
 }
+	
+

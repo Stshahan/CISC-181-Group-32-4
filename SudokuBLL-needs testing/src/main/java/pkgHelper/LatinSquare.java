f7f3e05857e523b2650eda79ac9effd87a0ced51
stsha <stsha@128.4.88.231>
2019-03-03 22:16:06 -0500

    isLatinSquare Complete, needs Testing

---
+++ SudokuBLL-needs testing/src/main/java/pkgHelper/LatinSquare.java
@@ -0,0 +1,203 @@
+package pkgHelper;
+
+import java.util.Arrays;
+
+
+public class LatinSquare {
+	
+/*
+ * Contains Zero : Complete
+ * doesElementExist Complete
+ * getColumn Complete
+ * getRow Complete
+ * hasAllValues Complete
+ * hasDuplicates Complete
+ * setLatinSquare Complete
+ * getLatinSquare Complete
+ * isLatinSquareComplete
+ */
+	
+	// test git
+	
+	private int[][] LatinSquare; // instance variable
+
+	public LatinSquare() {
+		//Constructor for LatinSquare (no arg)
+		super();
+			
+	}
+
+	
+	public LatinSquare(int[][] puzzle) {
+		//1 arg Constructor for LatinSquare
+		super();
+		this.LatinSquare = puzzle;
+	}
+	
+	public boolean isLatinSquare() {
+		
+		boolean isLatinSquare = true;
+		
+		
+		//Checks rows and Columns for Duplicates
+		for (int i = 0; i < this.LatinSquare.length; i++)
+		{
+			if (hasDuplicates(getRow(i)))
+			{ 
+				return false;
+			
+			}
+			
+			if (hasDuplicates(getColumn(i)))
+			{ 
+				return false;
+			
+			}
+			
+			
+		}
+		//returns false if rows do not contain all values
+		for (int i = 1; i < LatinSquare.length; i++) {
+			
+			if(!hasAllValues(getRow(0),getRow(i)))
+			{
+			return false;
+		}
+			if(!hasAllValues(getColumn(0),getColumn(i)))
+			{
+			return false;
+		}
+		
+		/*I don't think this part is necessary.
+		 * 
+		 * 
+		 * *LatinSquare Puz = new LatinSquare();
+		// need to separate into rows and columns
+		Puz.setLatinSquare(this.LatinSquare);
+		Puz.getLatinSquare();
+		Puz.ContainsZero();
+		*/
+		
+		
+		}
+		return isLatinSquare;
+	}
+	
+	
+
+	public int[][] getLatinSquare() {
+		// Method returns LatinSquare 2 dimensional array
+		return LatinSquare;
+	}
+
+	public void setLatinSquare(int[][] puzzle) {
+		//Method sets LatinSquare array
+		LatinSquare = puzzle;
+	}
+	
+	
+	public static boolean doesElementExist(int[] arr, int iValue)
+	//Searches for a specific element in the array(iValue) and returns true if it exists and false if it doesn't
+	{
+	
+	for(int x = 0; x < arr.length; x++) 
+		{
+	
+		if (arr[x] == iValue) {
+			return true;
+			}
+		}
+	return false;
+	}
+
+	public static boolean hasDuplicates (int[] arr)
+	{
+		int [] arrCopy = Arrays.copyOf(arr, arr.length);
+		
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < arr.length -1 ; i++) {
+			if (arrCopy[i] == arrCopy[i+1])
+			{
+				return true;
+			}
+			
+		}
+		
+		return false;
+		
+			
+	}
+	public boolean ContainsZero()
+	{
+		
+	for( int i = 0; i < LatinSquare.length; i++) {
+		for (int j = 0 ; j < LatinSquare.length; j++) {
+				if (LatinSquare[i][j] == 0 ){
+					return true;
+				}
+			}
+		}
+			
+	return false;
+	}
+	
+	
+	public int[] getColumn(int iCol)
+	{
+	// Method returns LatinSquare Column
+		int[] columnArray = new int [this.LatinSquare.length];
+	for (int Row=0; Row < this.LatinSquare.length; Row++) {
+			columnArray[Row] = LatinSquare[Row][iCol];
+		}
+		return columnArray;
+		}	
+
+	
+	public int[] getRow(int iRow)
+	{
+	// Method returns LatinSquare Row
+		int [] rowArray = new int [this.LatinSquare.length];
+	
+		rowArray = this.LatinSquare[iRow];
+	
+		return rowArray;
+	
+	}	
+	
+	public static boolean hasAllValues(int[] arr1, int[] arr2) {
+		// compares values from two arrays (Row and Column)
+		
+			
+		
+		
+		boolean hasAllValues = true;//assume true, unless proven otherwise
+		
+		if (arr1.length != arr2.length) {//tests the length of the arrays.
+			hasAllValues = false;
+		}
+		else {
+		for (int j = 0; j < arr2.length; j++) {
+
+		boolean bFound = false; //assumes false, unless loops prove otherwise.
+
+		for (int i = 0; i < arr1.length; i++) {
+		if (arr1[i] == arr2[j]) {
+		bFound = true;
+		break;
+		}
+
+		}
+		if (bFound == false) {
+		hasAllValues = false;
+		break;
+		}
+		}
+		}
+		return hasAllValues;
+
+		
+	}
+}
+		
+
---
+++ SudokuBLL-needs testing/src/test/java/pkgHelper/LatinSquareTest.java
@@ -0,0 +1,77 @@
+package pkgHelper;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+/*Tests completed:
+ * doesElementExistTests 1,2
+ */
+
+public class LatinSquareTest {
+
+	@Test
+	public void doesElementExist_Test() {
+		
+		int [] arr = {1,2,3,4,5};
+		int iValue = 5;
+		boolean bExpected = true;
+		boolean bActual = LatinSquare.doesElementExist(arr, iValue);
+		
+		assertEquals(bExpected, bActual);
+	}
+	@Test
+	public void doesElementExist_Test2() {
+		
+		int [] arr = {1,2,3,4,5};
+		int iValue = 6;
+		boolean bExpected = false;
+		boolean bActual = LatinSquare.doesElementExist(arr, iValue);
+		
+		assertEquals(bExpected, bActual);
+	}
+	
+	@Test
+	public void hasDuplicates_test1() {
+		int [] arr = {1,2,3,4,5};
+		boolean bExpected = false;
+		boolean bActual = LatinSquare.hasDuplicates(arr);
+		
+		assertEquals(bExpected, bActual);
+	}
+	@Test
+	public void hasDuplicates_test2() {
+		int [] arr = {1,2,3,4,4};
+		boolean bExpected = true;
+		boolean bActual = LatinSquare.hasDuplicates(arr);
+		
+		assertEquals(bExpected, bActual);
+	}
+	
+	@Test //Added by Ryan
+	public void hasAllValues_Test1() {
+		int [] tarr1 = {1,2,3,4,5};
+		int [] tarr2 = {1,2,3,4,5};
+		boolean expectedBool = true;
+		boolean actualBool = LatinSquare.hasAllValues(tarr1, tarr2);
+		
+		assertEquals(expectedBool,actualBool);
+		
+	}
+	@Test //Added by Ryan
+	public void hasAllValues_Test2() {
+		int [] tarr1 = {1,2,3,4};
+		int [] tarr2 = {1,2,3,6,5};
+		boolean expectedBool = false;
+		boolean actualBool = LatinSquare.hasAllValues(tarr1, tarr2);
+		
+		assertEquals(expectedBool,actualBool);
+		
+	}
+	
+	
+	
+}
+	
+
